5fa394bae37a70a243aa66159ec45e6d220ca022
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-19 19:35:51 -0700

    creates auth and auth router

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
-const express = requre('express');
+const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const passport = require('passport');
+// const passport = require('passport');
 
 const { PORT, HTTP_STATUS_CODES, MONGO_URL, TEST_MONGO_URL } = require('./config');
 
@@ -15,5 +15,54 @@ app.use(express.json());
 app.use(express.static('./public'));
 
 app.use('*', function(req, res){
-  res.status(HTTP_STATUS_CODES, NOT_FOUND).json({ error: 'Not found' });
+  res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ error: 'Not found' });
 });
+
+module.exports = {
+  app,
+  startServer,
+  stopServer
+};
+
+function startServer(testEnv){
+  return new Promise((resolve, reject) => {
+    let mongoUrl;
+    if(testEnv){
+      mongoUrl = TEST_MONGO_URL;
+    } else {
+      mongoUrl = MONGO_URL;
+    }
+    mongoose.connect(mongoUrl, { useNewUrlParser: true }, err => {
+      if(err){
+        console.error(err);
+        return reject(err);
+      } else {
+        server = app.listen (PORT, () => {
+          console.log(`Express server listening on http://localhost:${PORT}`);
+          resolve();
+        }).on('error', err => {
+          mongoose.disconnect();
+          console.error(err);
+          reject(err);
+        });
+      }
+    }); 
+  });
+};
+
+function stopServer(){
+  return mongoose
+    .disconnect()
+    .then(() => new Promise((resolve, reject) => {
+      server.close(err => {
+        if(err){
+          console.error(err);
+          return reject(err);
+        } else {
+          console.log('express server stopped');
+          resolve();
+        }
+      });
+    }))
+    .catch(err => console.error('err', err));
+};
