4b3d4b7b8579db210b017e8daa78a82ea3443955
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-03-20 15:27:53 -0700

    started testing

--- app/note/noteRouter.js
+++ app/note/noteRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Joi = require('Joi');
 const noteRouter = express.Router();
 const { HTTP_STATUS_CODE } = require('../config');
-const { jwtPassportMiddleware } = require('../auth/autStrategy');
+const { jwtPassportMiddleware } = require('../auth/authStrategy');
 const { Note, NoteJoiSchema } = require('./noteModel.js');
 
 // CREATE NEW NOTE
@@ -32,17 +32,59 @@ noteRouter.post("/", jwtPassportMiddleware, (req, res) => {
 });
   
 // RETRIEVE USER'S NOTES
-
 noteRouter.get('/', jwtPassportMiddleware, (req, res) => {
   Note.find({ user: req.user.id })
     .populate('user')
     .then(notes => {
       return res.status(HTTP_STATUS_CODE.OK).json(notes.map(note => note.serialize()));
     })
-    .catch(error => {return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(error);
+    .catch(error => {
+      return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(error);
     });
 });
 
+// RETRIEVE NOTE BY ID
+noteRouter.get('/:noteid', jwtPassportMiddleware, (req, res) => {
+  Note.findById(req.params.noteid)
+    .populate('user')
+    .then(note => {
+      return res.status(HTTP_STATUS_CODE.OK).json(note.serialize());
+    })
+    .catch(error => {
+      return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(error);
+    });
+});
 
+// DELETE NOTE BY ID
+noteRouter.delete('/:noteid', jwtPassportMiddleware, (req, res) => {
+  Note.findByIdAndDelete(req.params.noteid)
+    .then(() => {
+      return res.status(HTTP_STATUS_CODE.NO_CONTENT).end();
+    })
+    .catch(error => {
+      return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(error);
+    });
+});
+
+// UPDATE NOTE BY ID
+noteRouter.put('/:noteid', jwtPassportMiddleware, (req, res) => {
+  const noteUpdate ={
+    title: req.body.title,
+    content: req.body.content
+  };
+  const validation = Joi.validate(noteUpdate, NoteJoiSchema);
+  if (validation.error) {
+    return res
+      .status(HTTP_STATUS_CODES.BAD_REQUEST)
+      .json({ error: validation.error });
+  }
+  Note.findByIdAndUpdate(req.params.noteid, noteUpdate)
+    .then(() => {
+      return res.status(HTTP_STATUS_CODE.NO_CONTENT).end();
+    })
+  .catch(error => {
+    return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json(error);
+  })
+});
 
 module.exports = { noteRouter };
--- app/server.js
+++ app/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-const { PORT, HTTP_STATUS_CODES, MONGO_URL, TEST_MONGO_URL } = require('./config');
-const { authRouter } = require('./auth/authRouter');
-const { userRouter } = require('./user/userRouter');
-const { localStrategy, jwtStrategy } = require('./auth/authStrategy');
+const {
+  PORT,
+  HTTP_STATUS_CODES,
+  MONGO_URL,
+  TEST_MONGO_URL
+} = require("./config");
+const { authRouter } = require("./auth/authRouter");
+const { userRouter } = require("./user/userRouter");
+const { noteRouter } = require("./note/noteRouter");
+const { localStrategy, jwtStrategy } = require("./auth/authStrategy");
 
 let server;
 const app = express();
@@ -14,17 +20,20 @@ passport.use(localStrategy);
 passport.use(jwtStrategy);
 
 //middleware
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.static("./public"));
+
 //ROUTER SETUP
-app.use('api/auth', authRouter);
-app.use('api/user', userRouter);
+app.use("api/auth", authRouter);
+app.use("api/user", userRouter);
+app.use("api/note", noteRouter);
 
-
-//handle unhandled request instead of stopping serer
-app.use('*', function(req, res){
-  res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ error: 'Not found' });
+//handle unhandled request instead of crashing server
+app.use("*", function(req, res) {
+  res
+    .status(HTTP_STATUS_CODES.NOT_FOUND)
+    .json({ error: 'unhandled request error *: Not found' });
 });
 
 module.exports = {
@@ -33,45 +42,50 @@ module.exports = {
   stopServer
 };
 
-function startServer(testEnv){
+function startServer(testEnv) {
   return new Promise((resolve, reject) => {
     let mongoUrl;
-    if(testEnv){
+    if (testEnv) {
       mongoUrl = TEST_MONGO_URL;
     } else {
       mongoUrl = MONGO_URL;
     }
     mongoose.connect(mongoUrl, { useNewUrlParser: true }, err => {
-      if(err){
+      if (err) {
         console.error(err);
         return reject(err);
       } else {
-        server = app.listen (PORT, () => {
-          console.log(`Express server listening on http://localhost:${PORT}`);
-          resolve();
-        }).on('error', err => {
-          mongoose.disconnect();
-          console.error(err);
-          reject(err);
-        });
+        server = app
+          .listen(PORT, () => {
+            console.log(`Express server listening on http://localhost:${PORT}`);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            console.error(" connection error:", err);
+            reject(err);
+          });
       }
-    }); 
+    });
   });
-};
+}
 
-function stopServer(){
+function stopServer() {
   return mongoose
     .disconnect()
-    .then(() => new Promise((resolve, reject) => {
-      server.close(err => {
-        if(err){
-          console.error(err);
-          return reject(err);
-        } else {
-          console.log('express server stopped');
-          resolve();
-        }
-      });
-    }))
-    .catch(err => console.error('err', err));
-};
+    .then(
+      () =>
+        new Promise((resolve, reject) => {
+          server.close(err => {
+            if (err) {
+              console.error("sever close error: ", err);
+              return reject(err);
+            } else {
+              console.log("express server stopped");
+              resolve();
+            }
+          });
+        })
+    )
+    .catch(err => console.error("Stop server error:", err));
+}
