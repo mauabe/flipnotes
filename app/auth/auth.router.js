f5ef9f8cb44b61792f9df9425a50186dc33f3327
Mauricio <mauabe@gmx.com>
2019-05-13 19:43:32 -0700

    refactored auth and server

--- app/auth/auth.router.js
+++ app/auth/auth.router.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const jwt = require('jsonwebtoken');
 const {
   localPassportMiddleware,
   jwtPassportMiddleware
 } = require('../auth/auth.strategy')
-const { JWT_SECRET, JWT_EXPIRY } = require('../config.js')
+const { JWT_SECRET, JWT_EXPIRY } = require('../config.js');
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 function createJwtToken(user) {
   return jwt.sign({ user }, JWT_SECRET, {
@@ -14,18 +14,18 @@ function createJwtToken(user) {
     expiresIn: JWT_EXPIRY,
     algorithm: 'HS256'
   })
-}
+};
 
 authRouter.post('/login', localPassportMiddleware, (request, response) => {
   const user = request.user.serialize()
   const jwtToken = createJwtToken(user)
   response.json({ jwtToken, user })
-})
+});
 
 authRouter.post('/refresh', jwtPassportMiddleware, (request, response) => {
   const user = request.user
   const jwtToken = createJwtToken(user)
   response.json({ jwtToken, user })
-})
+});
 
-module.exports = { authRouter }
+module.exports = { authRouter };
